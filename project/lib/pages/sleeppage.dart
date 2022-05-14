f11ed6eb87445025baca0dbe8e3c3765d20f9ec6
Maria Ghirardi <maria.ghirardi@studenti.unipd.it>
2022-05-14 16:53:22 +0200

    fetch data sleep page

--- project/lib/pages/sleeppage.dart
+++ project/lib/pages/sleeppage.dart
@@ -1,7 +1,6 @@
 import 'package:fitbitter/fitbitter.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-
 import '../utils/strings.dart';
 
 class SleepPage extends StatelessWidget {
