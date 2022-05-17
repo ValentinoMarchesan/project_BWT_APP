76aec0addb729a22d9ee1b7596770470de1f790a
RiccardoZardin <100702517+RiccardoZardin@users.noreply.github.com>
2022-05-17 18:25:21 +0200

    implement allert for logout

--- project/lib/models/appbar_widget.dart
+++ project/lib/models/appbar_widget.dart
@@ -4,6 +4,7 @@ import 'package:project/pages/classes/UserPreferences.dart';
 import 'package:project/pages/loginPage.dart';
 import 'package:project/pages/profilePage.dart';
 import 'package:project/utils/strings.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 AppBar buildAppBar(BuildContext context) {
@@ -16,7 +17,51 @@ AppBar buildAppBar(BuildContext context) {
       IconButton(
           icon: Icon(Icons.logout),
           onPressed: () {
-            _toLoginPage(context);
+            Alert(
+                context: context,
+                title: 'LOGOUT',
+                desc: 'Do you want logout from your account',
+                style: AlertStyle(
+                  titleStyle: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    shadows: [
+                      Shadow(
+                          color: Color.fromARGB(255, 210, 239, 244),
+                          blurRadius: 2)
+                    ],
+                    letterSpacing: 2,
+                  ),
+                  descStyle: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 16,
+                    shadows: [
+                      Shadow(
+                          color: Color.fromARGB(255, 210, 239, 244),
+                          blurRadius: 2)
+                    ],
+                    letterSpacing: 2,
+                  ),
+                ),
+                buttons: [
+                  DialogButton(
+                    radius: const BorderRadius.all(Radius.circular(30)),
+                    color: Colors.orangeAccent,
+                    child: Text('No'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  DialogButton(
+                    radius: const BorderRadius.all(Radius.circular(30)),
+                    color: Colors.orangeAccent,
+                    child: Text('Yes'),
+                    onPressed: () {
+                      _toLoginPage(context);
+                    },
+                  )
+                ]).show();
           })
     ],
   );
